9f23daab659545886425f7cec53f1d86f2b6b0c4
Tuangpi <singkhantuang97@gmail.com>
2023-10-20 14:48:12 +0630

    commit3

--- app.js
+++ app.js
@@ -1,41 +1,4 @@
-// const video = document.getElementById('video');
-// const switchCameraButton = document.getElementById('switchCamera');
-// let currentStream;
-// let currentDeviceIndex = 0;
-
-// async function getVideoDevices() {
-//   const devices = await navigator.mediaDevices.enumerateDevices();
-//   return devices.filter(device => device.kind === 'videoinput');
-// }
-
-// async function initCamera(deviceId) {
-//   try {
-//     if (currentStream) {
-//       currentStream.getTracks().forEach(track => track.stop());
-//     }
-
-//     const constraints = { video: { deviceId: { exact: deviceId } } };
-//     const stream = await navigator.mediaDevices.getUserMedia(constraints);
-//     video.srcObject = stream;
-//     currentStream = stream;
-//   } catch (error) {
-//     console.error('Error accessing the camera: ', error);
-//   }
-// }
-
-// async function toggleCamera() {
-//   const devices = await getVideoDevices();
-//   if (devices.length < 2) {
-//     console.error('No other camera available.');
-//     return;
-//   }
-
-//   currentDeviceIndex = (currentDeviceIndex + 1) % devices.length;
-//   const nextDeviceId = devices[currentDeviceIndex].deviceId;
-//   initCamera(nextDeviceId);
-// }
-
-// switchCameraButton.addEventListener('click', toggleCamera);
+const sourceSelect = document.getElementById('switchCamera')
 
 function decodeOnce(codeReader, selectedDeviceId) {
   codeReader.decodeFromInputVideoDevice(selectedDeviceId, 'video').then((result) => {
@@ -55,9 +18,8 @@ window.addEventListener('load', function () {
 
   codeReader.getVideoInputDevices()
     .then((videoInputDevices) => {
-      const sourceSelect = document.getElementById('switchCamera')
+
       selectedDeviceId = videoInputDevices[0].deviceId
-      console.log(videoInputDevices)
 
       if (videoInputDevices.length > 1) {
         sourceSelect.onclick = () => {
@@ -66,10 +28,7 @@ window.addEventListener('load', function () {
         };
       }
 
-      document.getElementById('startButton').addEventListener('click', () => {
-        document.getElementById('result').textContent = 'startbutton clicked' + selectedDeviceId;
-        decodeOnce(codeReader, selectedDeviceId);
-      })
+      decodeOnce(codeReader, selectedDeviceId);
 
       document.getElementById('resetButton').addEventListener('click', () => {
         codeReader.reset()
