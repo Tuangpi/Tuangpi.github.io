8be639cf0a213d40b681e9e4602e72b84e41ca68
Tuangpi <singkhantuang97@gmail.com>
2023-10-20 15:14:26 +0630

    commit3

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-const sourceSelect = document.getElementById('switchCamera')
+const switchCamera = document.getElementById('switchCamera')
 
 function decodeOnce(codeReader, selectedDeviceId) {
   codeReader.decodeFromInputVideoDevice(selectedDeviceId, 'video').then((result) => {
-    console.log(result, 'decode result')
-    document.getElementById('result').textContent = result.text + ', ' + selectedDeviceId
+    document.getElementById('result').textContent = result.text
   }).catch((err) => {
-    console.error(err)
-    document.getElementById('result').textContent = err + 'decode one'
+    document.getElementById('result').textContent = err + 'inside decodeOnce'
   })
 }
 
@@ -19,12 +17,18 @@ window.addEventListener('load', function () {
   codeReader.getVideoInputDevices()
     .then((videoInputDevices) => {
 
-      selectedDeviceId = videoInputDevices[1].deviceId
+      selectedDeviceId = videoInputDevices[0].deviceId
 
       if (videoInputDevices.length > 1) {
-        sourceSelect.onclick = () => {
-          selectedDeviceId = videoInputDevices[1].deviceId;
-          document.getElementById('result').textContent = 'switchbutton ' + videoInputDevices.length;
+        selectedDeviceId = videoInputDevices[1].deviceId
+
+        switchCamera.onclick = () => {
+
+          if (selectedDeviceId = videoInputDevices[1]) {
+            selectedDeviceId = videoInputDevices[0].deviceId
+          } else {
+            selectedDeviceId = videoInputDevices[1].deviceId
+          }
         };
       }
 
